3e15843dc82e2883a93a7a5f5f5ead0c18ec17be
Kees Bakker <kees@sodaq.net>
2013-11-09 15:00:56 +0100

    Change to LGPL 3 and add COPYING and COPYING.LESSER

--- SHT2x.h
+++ SHT2x.h
@@ -11,20 +11,22 @@
     - code optimisation
     - compatibility with Arduino 1.0
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * This file is part of Sodaq_SHT2x.
+ *
+ * Sodaq_SHT2x is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published bythe Free Software Foundation, either version 3 of
+ * the License, or(at your option) any later version.
+ *
+ * Sodaq_SHT2x is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sodaq_SHT2x.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 
 #ifndef SHT2X_H
