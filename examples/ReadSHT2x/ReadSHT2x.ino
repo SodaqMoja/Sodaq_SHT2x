ab8c599ec79089f6d00101d63520785733271e1a
Kees Bakker <kees@m2m4all.com>
2015-12-26 18:33:20 +0100

    Add GetDewPoint to the example

--- examples/ReadSHT2x/ReadSHT2x.ino
+++ examples/ReadSHT2x/ReadSHT2x.ino
@@ -24,6 +24,8 @@ void loop()
   Serial.print(SHT2x.GetHumidity());
   Serial.print("     Temperature(C): ");
   Serial.println(SHT2x.GetTemperature());
+  Serial.print("     Dewpoint(C): ");
+  Serial.println(SHT2x.GetDewPoint());
   
   delay(1000);
 }
